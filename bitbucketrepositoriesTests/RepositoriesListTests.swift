b761dbe1ce6958a1cc40a3225d7ed7b971452d19
Ezaden <ezadens@yahoo.com>
2021-05-30 22:42:49 +0800

    Add test for next page api response

--- bitbucketrepositoriesTests/RepositoriesListTests.swift
+++ bitbucketrepositoriesTests/RepositoriesListTests.swift
@@ -30,4 +30,24 @@ class RepositoriesListTests: XCTestCase {
         
         self.wait(for: [expectation], timeout: 30)
     }
+    
+    func testNextPageAPIResponse() {
+        let provider = RepositoriesProvider(with: APIService())
+        
+        let expectation = XCTestExpectation(description: "HTTP response expected to fail")
+        
+        provider.getNexPage(from: "https://www.apple.com") { [weak self] result in
+            guard let _ = self else {return}
+            switch result {
+            case .success(_):
+                XCTFail("Request did not fail when it was expected to.")
+            case .failure(let error):
+                XCTAssertNotNil(error.localizedDescription, "API response failed")
+            }
+            
+            expectation.fulfill()
+        }
+        
+        self.wait(for: [expectation], timeout: 30)
+    }
 }
