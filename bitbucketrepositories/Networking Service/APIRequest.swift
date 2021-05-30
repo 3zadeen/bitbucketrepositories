7d7d864481aefb3488f0fb9e575b0b3a04da2923
Ezaden <ezadens@yahoo.com>
2021-05-30 20:21:16 +0800

    Add URL components builder

--- bitbucketrepositories/Networking Service/APIRequest.swift
+++ bitbucketrepositories/Networking Service/APIRequest.swift
@@ -6,3 +6,38 @@
 //
 
 import Foundation
+
+struct APIRequest {
+    private var urlComponents = URLComponents()
+    
+    init() {
+        urlComponents.scheme = "https"
+        urlComponents.host = "api.bitbucket.org"
+    }
+}
+
+extension APIRequest {
+    var path: String {
+        get {
+            urlComponents.path
+        }
+        
+        set {
+            urlComponents.path = newValue
+        }
+    }
+    
+    var queryItems: [URLQueryItem]? {
+        get {
+            urlComponents.queryItems
+        }
+        
+        set {
+            urlComponents.queryItems = newValue
+        }
+    }
+    
+    var url: URL? {
+        urlComponents.url
+    }
+}
