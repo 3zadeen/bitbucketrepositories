a0076a9667e0285815aff1c0a836d2e4a75dc142
Ezaden <ezadens@yahoo.com>
2021-05-30 21:18:07 +0800

    Configure views inside table view cell

--- bitbucketrepositories/View/Cells/RepositoryCell.swift
+++ bitbucketrepositories/View/Cells/RepositoryCell.swift
@@ -24,13 +24,66 @@ class RepositoryCell: UITableViewCell {
         return image
     }()
     
+    let displayNameLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont(name: "HelveticaNeue", size: 18)
+        label.textAlignment = .left
+        label.textColor = UIColor.black
+        return label
+    }()
+    
+    let typeLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont(name: "HelveticaNeue", size: 18)
+        label.textAlignment = .left
+        label.textColor = UIColor.black
+        return label
+    }()
+    
+    let dateOfCreationLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont(name: "HelveticaNeue", size: 18)
+        label.textAlignment = .left
+        label.textColor = UIColor.black
+        return label
+    }()
+    
     func setupView() {
         addSubview(avatar)
+        addSubview(displayNameLabel)
+        addSubview(dateOfCreationLabel)
+        addSubview(typeLabel)
+
         avatar.anchor(top: topAnchor,
                       leading: leadingAnchor,
                       bottom: nil,
                       trailing: nil,
                       padding: UIEdgeInsets(top: 8, left: 16, bottom: 8, right: 0),
                       size: CGSize(width: 60, height: 60))
+        
+        displayNameLabel.anchor(top: topAnchor,
+                                leading: avatar.trailingAnchor,
+                                bottom: nil,
+                                trailing: trailingAnchor,
+                                padding: UIEdgeInsets(top: 16, left: 12, bottom: 5, right: 5),
+                                size: CGSize(width: 0, height: 0))
+        
+        dateOfCreationLabel.anchor(top: avatar.bottomAnchor,
+                                   leading: avatar.leadingAnchor,
+                                   bottom: bottomAnchor,
+                                   trailing: nil,
+                                   padding: UIEdgeInsets(top: 8, left: 0, bottom: 8, right: 0),
+                                   size: CGSize(width: 0, height: 0))
+        
+        typeLabel.anchor(top: dateOfCreationLabel.topAnchor,
+                         leading: dateOfCreationLabel.trailingAnchor,
+                         bottom: bottomAnchor,
+                         trailing: trailingAnchor,
+                         padding: UIEdgeInsets(top: 8, left: 8, bottom: 8, right: 16),
+                         size: CGSize(width: 0, height: 0))
+    }
+    
+    override func layoutSubviews() {
+        avatar.layer.cornerRadius = avatar.bounds.width / 2
     }
 }
