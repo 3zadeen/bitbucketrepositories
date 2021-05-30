2a19f4aba070fb80d22429e20da1c2a0f7152ffe
Ezaden <ezadens@yahoo.com>
2021-05-30 20:08:56 +0800

    Add endpoint that returns url request to fetch repositories

--- bitbucketrepositories/Networking Service/Endpoint.swift
+++ bitbucketrepositories/Networking Service/Endpoint.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+
+enum Endpoint {
+    case repositories
+}
+
+extension Endpoint: RequestProviding {
+    var urlRequest: URLRequest {
+        switch self {
+        case .repositories:
+            var apiRequest = APIRequest()
+            apiRequest.path = "/2.0/repositories"
+            
+            guard let url = apiRequest.url else {
+                preconditionFailure("Error: Invalid URL used to create URL instance")
+            }
+            
+            return URLRequest(url: url)
+        }
+    }
+}
