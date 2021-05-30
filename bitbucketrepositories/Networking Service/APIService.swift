da7b3a7dfa2b90aae71ec118b09fdb6d7a2794b3
Ezaden <ezadens@yahoo.com>
2021-05-30 20:18:05 +0800

    Add a function to fetch data from API

--- bitbucketrepositories/Networking Service/APIService.swift
+++ bitbucketrepositories/Networking Service/APIService.swift
@@ -6,3 +6,36 @@
 //
 
 import Foundation
+
+protocol Networking {
+    func fetchData<T: Decodable>(_ endPoint: Endpoint, completion: @escaping (Result<T, Error>) -> Void)
+}
+
+protocol RequestProvider {
+    var urlRequest: URLRequest { get }
+}
+
+class APIService: Networking {
+    func fetchData<T: Decodable>(_ endPoint: Endpoint, completion: @escaping (Result<T, Error>) -> Void) {
+        let urlRequest = endPoint.urlRequest
+
+        URLSession.shared.dataTask(with: urlRequest) { data, response, error in
+            do {
+                if let error = error {
+                    completion(.failure(error))
+                    return
+                }
+
+                guard let data = data else {
+                    preconditionFailure("No error was received but we also don't have data...")
+                }
+
+                let decodedObject = try JSONDecoder().decode(T.self, from: data)
+
+                completion(.success(decodedObject))
+            } catch {
+                completion(.failure(error))
+            }
+        }.resume()
+    }
+}
