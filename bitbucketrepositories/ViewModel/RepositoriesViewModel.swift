df61eebc48a4cc797fe5f16d87a41ac2d4f82776
Ezaden <ezadens@yahoo.com>
2021-05-30 21:07:47 +0800

    Add functions to fetch repositories

--- bitbucketrepositories/ViewModel/RepositoriesViewModel.swift
+++ bitbucketrepositories/ViewModel/RepositoriesViewModel.swift
@@ -7,11 +7,58 @@
 
 import Foundation
 
+enum NetworkingError: Error {
+    case notFound(error: Error)
+}
+
+extension NetworkingError: LocalizedError {
+    var description: String {
+        switch self {
+        case .notFound(let error):
+            return  NSLocalizedString(error.localizedDescription, comment: "Data not found!, check if data is decoded correctly")
+        }
+    }
+}
+
 class RepositoriesViewModel {
 
     let provider: RepositoriesProvider
     
+    private(set) var repositories: [Repository]?
+    
+    private(set) var nextPageURL: String?
+    
     init(with provider: RepositoriesProvider) {
         self.provider = provider
+    }
+    
+    func getRepositories(completion: @escaping (Error?) -> Void) {
+        provider.getRepositories { [weak self] result in
+            guard let self = self else {return}
+            
+            switch result {
+            case .success(let repositories):
+                self.repositories = repositories.repositories
+                self.nextPageURL = repositories.next
+                completion(nil)
+            case .failure(let error):
+                completion(NetworkingError.notFound(error: error))
+            }
+        }
+    }
+    
+    func getNextPage(from urlString: String, completion: @escaping (Error?) -> Void) {
+        provider.getNexPage(from: urlString) { [weak self] result in
+            guard let self = self else {return}
+            
+            switch result {
+            case .success(let repositories):
+                self.repositories = repositories.repositories
+                self.nextPageURL = repositories.next
+                completion(nil)
+            case .failure(let error):
+                completion(NetworkingError.notFound(error: error))
+            }
+        }
     }
 }
