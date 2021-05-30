8bf6f811a03b3581fdeb753bf032d31833671320
Ezaden <ezadens@yahoo.com>
2021-05-30 21:06:32 +0800

    Add image view for repository avatar

--- bitbucketrepositories/View/Cells/RepositoryCell.swift
+++ bitbucketrepositories/View/Cells/RepositoryCell.swift
@@ -18,7 +18,19 @@ class RepositoryCell: UITableViewCell {
         fatalError("init(coder:) has not been implemented")
     }
     
+    let avatar: ImageView = {
+        let image = ImageView()
+        image.clipsToBounds = true
+        return image
+    }()
+    
     func setupView() {
-        
+        addSubview(avatar)
+        avatar.anchor(top: topAnchor,
+                      leading: leadingAnchor,
+                      bottom: nil,
+                      trailing: nil,
+                      padding: UIEdgeInsets(top: 8, left: 16, bottom: 8, right: 0),
+                      size: CGSize(width: 60, height: 60))
     }
 }
