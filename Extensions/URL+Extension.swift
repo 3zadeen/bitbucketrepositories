a238c6dda2d7e7dc7253d7d7c19a64169e3c34ac
Ezaden <ezadens@yahoo.com>
2021-05-30 20:30:29 +0800

    Add function to get query item from query paramater

--- Extensions/URL+Extension.swift
+++ Extensions/URL+Extension.swift
@@ -6,3 +6,10 @@
 //
 
 import Foundation
+
+extension URL {
+    func valueOf(_ queryParamName: String) -> String? {
+        guard let url = URLComponents(string: self.absoluteString) else { return nil }
+        return url.queryItems?.first(where: { $0.name == queryParamName })?.value
+    }
+}
