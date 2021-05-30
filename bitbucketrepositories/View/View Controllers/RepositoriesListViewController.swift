3f34034e0295d0ea1b3fdb034eb148eb9641c9a9
Ezaden <ezadens@yahoo.com>
2021-05-30 19:54:01 +0800

    Inject view model to view controller

--- bitbucketrepositories/View/View Controllers/RepositoriesListViewController.swift
+++ bitbucketrepositories/View/View Controllers/RepositoriesListViewController.swift
@@ -9,11 +9,20 @@ import UIKit
 
 class RepositoriesListViewController: BaseViewController<RepositoriesListView> {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    let viewModel: RepositoriesViewModel
+    
+    init(with viewModel: RepositoriesViewModel) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
 
+    override func viewDidLoad() {
+        super.viewDidLoad()
 
+    }
 }
 
