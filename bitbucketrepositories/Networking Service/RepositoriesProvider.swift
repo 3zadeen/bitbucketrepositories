24454c92a6fa69d106bb8beef67ccfa0bf13b9d1
Ezaden <ezadens@yahoo.com>
2021-05-30 20:25:03 +0800

    Add a function to fetch repositories from API

--- bitbucketrepositories/Networking Service/RepositoriesProvider.swift
+++ bitbucketrepositories/Networking Service/RepositoriesProvider.swift
@@ -6,3 +6,22 @@
 //
 
 import Foundation
+
+enum Result<Success, Failure> where Failure: Error {
+    case success(Success)
+    case failure(Failure)
+}
+
+class RepositoriesProvider {
+    typealias Completion<T> = (Result<T, Error>) -> Void
+
+    let service: APIService
+    
+    init(with networking: APIService) {
+        self.service = networking
+    }
+    
+    func getRepositories(completion: @escaping Completion<Repositories>) {
+        service.fetchData(.repositories, completion: completion)
+    }
+}
