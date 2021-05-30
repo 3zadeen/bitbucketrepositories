44005960089ca27bb2c1b8a3e4e521f95fe67614
Ezaden <ezadens@yahoo.com>
2021-05-30 20:39:11 +0800

    Add UITableview cell identifier to extension

---
+++ Extensions/UITableViewCell+Extension.swift
@@ -0,0 +1,14 @@
+//
+//  UITableViewCell+Extension.swift
+//  bitbucketrepositories
+//
+//  Created by Ezaden Seraj on 30/5/21.
+//
+
+import UIKit
+
+extension UITableViewCell {
+    static var identifier: String {
+        return String(describing: self.self)
+    }
+}
