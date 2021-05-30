163729d3c3e993ac7d59de93500d8b5fb556df0b
Ezaden <ezadens@yahoo.com>
2021-05-30 19:17:43 +0800

    Add table view in repositories list view

--- bitbucketrepositories/View/Views/RepositoriesListView.swift
+++ bitbucketrepositories/View/Views/RepositoriesListView.swift
@@ -9,8 +9,23 @@ import UIKit
 
 class RepositoriesListView: BaseView {
     
+    let tableview: UITableView = {
+        let tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        return tableView
+    }()
+
+    
     override func setViews() {
         super.setViews()
         
+        addSubview(tableview)
+
+        tableview.anchor(top: self.safeAreaLayoutGuide.topAnchor,
+                         leading: self.safeAreaLayoutGuide.leadingAnchor,
+                         bottom: self.safeAreaLayoutGuide.bottomAnchor,
+                         trailing: self.safeAreaLayoutGuide.trailingAnchor,
+                         padding: UIEdgeInsets(top: 0, left: 0, bottom: 50, right: 0),
+                         size: CGSize(width: 0, height: 0))
     }
 }
