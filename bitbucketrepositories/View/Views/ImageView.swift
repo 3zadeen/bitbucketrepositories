d10e82bb573b298f5c5bb333cc57c7e834283abc
Ezaden <ezadens@yahoo.com>
2021-05-30 21:45:33 +0800

    Add a function to download image and cache

--- bitbucketrepositories/View/Views/ImageView.swift
+++ bitbucketrepositories/View/Views/ImageView.swift
@@ -8,5 +8,35 @@
 import UIKit
 
 class ImageView: UIImageView {
+    let cachedImage = NSCache<NSString, UIImage>()
     
+    func load(from stringURL: String, with contentMode: UIView.ContentMode) {
+        if let cachedImage = cachedImage.object(forKey: stringURL as NSString) {
+            self.image = cachedImage
+            self.contentMode =  contentMode
+            
+            return
+        }
+        
+        guard let url = URL(string: stringURL) else {
+            return
+        }
+        
+        DispatchQueue.global().async { [weak self] in
+            guard let self = self else {
+                return
+            }
+
+            if let data = try? Data(contentsOf: url) {
+                if let image = UIImage(data: data) {
+                    DispatchQueue.main.async {
+                        self.cachedImage.setObject(image, forKey: stringURL as NSString)
+                        self.image = image
+                    }
+                }
+            } else {
+                self.cachedImage.setObject(UIImage(), forKey: stringURL as NSString)
+            }
+        }
+    }
 }
