aeab6b0502e4b4c6c9f9593da1f35b3d1626ac0c
Ezaden <ezadens@yahoo.com>
2021-05-30 19:22:41 +0800

    Add tool bar to the bottom of the page

--- bitbucketrepositories/View/Views/RepositoriesListView.swift
+++ bitbucketrepositories/View/Views/RepositoriesListView.swift
@@ -15,6 +15,13 @@ class RepositoriesListView: BaseView {
         return tableView
     }()
 
+    let toolBar: UIToolbar = {
+        let toolbar = UIToolbar()
+        let leftSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let nextPageButton = UIBarButtonItem(title: "Next Page", style: .plain, target: self, action: #selector(loadNextPage))
+        toolbar.setItems([leftSpace, nextPageButton], animated: true)
+        return toolbar
+    }()
     
     override func setViews() {
         super.setViews()
@@ -27,5 +34,14 @@ class RepositoriesListView: BaseView {
                          trailing: self.safeAreaLayoutGuide.trailingAnchor,
                          padding: UIEdgeInsets(top: 0, left: 0, bottom: 50, right: 0),
                          size: CGSize(width: 0, height: 0))
+        
+        toolBar.anchor(top: nil,
+                       leading: self.safeAreaLayoutGuide.leadingAnchor,
+                       bottom: self.safeAreaLayoutGuide.bottomAnchor,
+                       trailing: self.safeAreaLayoutGuide.trailingAnchor)
+    }
+    
+    @objc func loadNextPage() {
+        print("next page")
     }
 }
